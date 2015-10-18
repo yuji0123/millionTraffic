17ecb556662f67243d72e0e74882aa9ff92a2114
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-10-18 16:20:49 +0900

    生クエリ

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,36 +1,78 @@
 class SearchController < ApplicationController
+
+
 	def findby
-
 		@orders = Order.all
-
 		if params[:findByOrderDateTimeGTE] != nil then
-			# @orders = Order.find_by_sql(['select * from orders where orderDateTime >= ? order by orderDateTime desc limit ?', params[:findByOrderDateTimeGTE].to_i, params[:limit].to_i])
-			@orders = @orders.where('orderDateTime >= ?', params[:findByOrderDateTimeGTE].to_i).order('orderDateTime desc')
+			query_str = ''
+			query_str << 'select * from orders where orders.orderDateTime >= '
+			query_str << params[:findByOrderDateTimeGTE]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
+
 		end
 		if params[:findByOrderDateTimeLTE] != nil then
-			@orders = @orders.where('orderDateTime <= ?', params[:findByOrderDateTimeLTE].to_i).order('orderDateTime desc')
+			query_str = ''
+			query_str << 'select * from orders where orders.orderDateTime <= '
+			query_str << params[:findByOrderDateTimeLTE]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
 		end
 		if params[:findByOrderUserId] != nil then
-			@orders = @orders.where(orderUserId: params[:findByOrderUserId])
+			query_str = ''
+			query_str << 'select * from orders where orders.orderUserId = '
+			query_str << params[:findByOrderUserId]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
 		end
 		if params[:findByOrderItemId] != nil then
-			@orders = @orders.where(orderItemId: params[:findByOrderItemId])
+			query_str = ''
+			query_str << 'select * from orders where orders.orderItemId = '
+			query_str << params[:findByOrderUserId]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
 		end
 		if params[:findByOrderQuantityGTE] != nil then
-			@orders = @orders.where('orderQuantity >= ?', params[:findByOrderQuantityGTE].to_i).order('orderDateTime desc')
+			query_str = ''
+			query_str << 'select * from orders where orders.orderQuantity >= '
+			query_str << params[:findByOrderQuantityGTE]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
 		end
 		if params[:findByOrderQuantityLTE] != nil then
-			@orders = @orders.where('orderQuantity <= ?', params[:findByOrderQuantityLTE].to_i).order('orderDateTime desc')
+			query_str = ''
+			query_str << 'select * from orders where orders.orderQuantity >= '
+			query_str << params[:findByOrderQuantityLTE]
+			query_str << ' order by orderDateTime desc limit '
+			query_str << params[:limit]
+			@orders = @orders.find_by_sql(query_str)
 		end
 		if params[:findByOrderState] != nil then
 			@orders = @orders.where(orderState: params[:findByOrderState])
 		end
 
 		if params[:findByOrderTagsIncludeAll] != nil then
+			query_str = ''
+			query_str << 'select * from orders '
 			tags = params[:findByOrderTagsIncludeAll].split(",")
+			lastTag = tags.last
 			tags.each{|tag|
-				@orders = @orders.where("orderTags like '%" + tag + "%'")
+				_tag = tag
+				query_str << 'where orders.orderTags '
+				query_str << 'like "%'
+				query_str << _tag
+				query_str << '%"'
+				if _tag != lastTag then
+					query_str << ' or '
+				end
 			}
+			query_str << ' limit '
+			query_str << params[:limit]
 		end
 
 		if params[:findByOrderTagsIncludeAny] != nil then
@@ -68,9 +110,9 @@ class SearchController < ApplicationController
 		# 		order.orderTags = order.orderTags.split(',')
 		# 	}
 		# end
-		if params[:limit] != nil then
-				@orders = @orders.limit(params[:limit].to_i)
-		end
+		# if params[:limit] != nil then
+		# 		@orders = @orders.limit(params[:limit].to_i)
+		# end
 
 
     	  @result = true
--- app/views/search/findby.json.jbuilder
+++ app/views/search/findby.json.jbuilder
@@ -2,14 +2,14 @@ json.set! "result", @result
 if @result then
 	json.set! "data" do
 		json.array! @orders do |order|
-		    json.orderId order.orderId
-		    json.orderDateTime order.orderDateTime
-		    json.orderUserId order.orderUserId
-		    json.orderItemId order.orderItemId
-		    json.orderQuantity order.orderQuantity
-		    json.orderState order.orderState
+		    json.orderId order["orderId"]
+		    json.orderDateTime order["orderDateTime"]
+		    json.orderUserId order["orderUserId"]
+		    json.orderItemId order["orderItemId"]
+		    json.orderQuantity order["orderQuantity"]
+		    json.orderState order["orderState"]
 		    json.orderTags do
-			    json.array! (order.orderTags.split(','))
+			    json.array! (order["orderTags"].split(','))
 			end
 		end
 	end
