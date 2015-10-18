ed5a87c7734d88a21ecbe4e6729bf474a36f89c8
Yuji Koketsu <kyuji7123.public@gmail.com>
2015-10-18 12:55:57 +0900

    クエリ変更

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
 	def findby
 		if params[:findByOrderDateTimeGTE] != nil then
-			@orders = Order.where('orderDateTime >= ?', params[:findByOrderDateTimeGTE].to_i).order('orderDateTime desc')
-			if params[:limit] != nil then
-				@orders = @orders.limit(params[:limit].to_i)
-			else
-				@orders = @orders.limit(100)
-			end
+			@orders = Order.find_by_sql(['select * from orders where orderDateTime >= ? order by orderDateTime desc limit ?', params[:findByOrderDateTimeGTE].to_i, params[:limit].to_i])
+			# @orders = Order.where('orderDateTime >= ?', params[:findByOrderDateTimeGTE].to_i).order('orderDateTime desc')
+			# if params[:limit] != nil then
+			# 	@orders = @orders.limit(params[:limit].to_i)
+			# else
+			# 	@orders = @orders.limit(100)
+			# end
 		end
 		if params[:findByOrderDateTimeLTE] != nil then
 			@orders = Order.where('orderDateTime <= ?', params[:findByOrderDateTimeLTE].to_i).order('orderDateTime desc')
@@ -82,6 +83,27 @@ class SearchController < ApplicationController
 				@orders = @orders.limit(100).order('orderDateTime desc')
 			end
 		end
+
+		if params[:findByUserCompany] != nil then
+			users = User.where(userCompany: params[:userCompany])
+			userId = users.arel_table[:userId]
+			@orders = Order.where(orderUserId: userId).order('orderDateTime desc').limit(params[:limit].to_i)
+		end
+
+		if params[:findByUserDiscountRageGTE] != nil then
+			users = User.where('userDiscountRate >= ?', params[:userDiscountRate])
+			users.each{|user|
+				userId = user.arel_table[:userId]
+			}
+			@orders = Order.where(orderUserId: user.userId).order('orderDateTime desc').limit(params[:limit])
+		end
+
+		if params[:findByUserDiscountRageGTE] != nil then
+			user = User.where('userDiscountRate <= ?', params[:userDiscountRate])
+			@orders = Order.where(orderUserId: user.userId).order('orderDateTime desc').limit(params[:limit])
+		end
+
+
 
 		# if @orders != nil then
 		# 	@orders.each{|order|
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,3 @@
 class Order < ActiveRecord::Base
-	def to_builder
-		Jbuilder.new do |order|
-			order.(self, :orderId, :orderDateTime, :orderUserId, :orderItemId, :orderQuantity, :orderState)
-		end
 
-	end
 end
